6587a4958860a128644990464b680e403cc06a0c
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-27 08:47:13 +0700

    chore: init

---
+++ csvappfuture/src/main/java/com/ardwiinoo/csvappfuture/CsvappfutureApplication.java
@@ -0,0 +1,13 @@
+package com.ardwiinoo.csvappfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvappfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvappfutureApplication.class, args);
+	}
+
+}
---
+++ csvappfuture/src/test/java/com/ardwiinoo/csvappfuture/CsvappfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardwiinoo.csvappfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvappfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
