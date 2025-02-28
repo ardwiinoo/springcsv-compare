3b7b48212e4ab169a97a97e3bddd0147f0d7374f
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-28 14:44:32 +0700

    feat: add CountryRepository for managing Country entities in the database

---
+++ csvapp/src/main/java/com/ardwiinoo/csvapp/repositories/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.ardwiinoo.csvapp.repositories;
+
+import com.ardwiinoo.csvapp.models.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+}
